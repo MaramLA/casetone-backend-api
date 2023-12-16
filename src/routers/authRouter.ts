7173f6d0c91c3b1a8c409a73e5fffd737cb96bde
MaramLA <maramalfaraj2000@gmail.com>
2023-12-16 21:45:44 +0300

    nothing special

--- src/routers/authRouter.ts
+++ src/routers/authRouter.ts
@@ -11,8 +11,15 @@ import { userLoginValidation } from '../validation/userValidation'
 const router = express.Router()
 
 //GET --> login by email and password
-router.post('/login', isLoggedOut, rateLimitMiddleware,userLoginValidation, runValidation,controller.login)
+router.post(
+  '/login',
+  isLoggedOut,
+  rateLimitMiddleware,
+  userLoginValidation,
+  runValidation,
+  controller.login
+)
 //GET --> logout
-router.post('/logout', isLoggedIn, controller.logout)
+router.post('/logout', controller.logout)
 
 export default router
